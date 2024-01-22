033747d55740d3026cca7a2ad15d264a8d1a398d
Akshay <41481611+Descended@users.noreply.github.com>
2024-01-22 18:37:50 +0100

    FEAT: FIELD Constants for game

--- src/main/java/henesys/constants/GameConstants.java
+++ src/main/java/henesys/constants/GameConstants.java
@@ -8,6 +8,8 @@ public class GameConstants {
     public static final int RANDOM_EQUIP_EPIC_CHANCE = 3; // out of a 100
     public static final int RANDOM_EQUIP_RARE_CHANCE = 8; // out of a 100
 
+    public static final int DEFAULT_FIELD_MOB_CAPACITY = 100;
+    public static final double DEFAULT_FIELD_MOB_RATE_BY_MOBGEN_COUNT = 1.5;
     public static final int TRUNK_SLOT_COUNT = 4;
     public static final boolean OVERRIDE_MAX_CONSUMABLES = true;
     public static boolean isValidName(String name) {
