b8a8d06d08a66cb9a013ca82ac474423d420bf1a
Akshay <41481611+Descended@users.noreply.github.com>
2024-01-23 00:08:09 +0100

    FEAT: Drop & Money constants

--- src/main/java/henesys/constants/GameConstants.java
+++ src/main/java/henesys/constants/GameConstants.java
@@ -12,6 +12,9 @@ public class GameConstants {
     public static final double DEFAULT_FIELD_MOB_RATE_BY_MOBGEN_COUNT = 1.5;
     public static final int TRUNK_SLOT_COUNT = 4;
     public static final boolean OVERRIDE_MAX_CONSUMABLES = true;
+    public static final long MAX_MONEY = 9_999_999_999L;
+    public static final int MAX_DROP_CHANCE = 10000;
+
     public static boolean isValidName(String name) {
         return name.length() >= 4 && name.length() <= 13 && Util.isDigitLetterString(name);
     }
