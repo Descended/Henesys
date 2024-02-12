5ceab7aa52e54994fd5bf3e26df1dc5d8f3cf007
Akshay <41481611+Descended@users.noreply.github.com>
2024-02-12 22:23:56 +0100

    FIX: Incorrect npc encoding

--- src/main/java/henesys/life/npc/Npc.java
+++ src/main/java/henesys/life/npc/Npc.java
@@ -62,7 +62,7 @@ public class Npc extends Life {
     public void encode(OutPacket outPacket) {
         // CNpc::Init
         outPacket.encodePosition(getPosition());
-        outPacket.encodeByte(isMove());
+        outPacket.encodeByte(!isFlip());
         outPacket.encodeShort(getFh());
         outPacket.encodeShort(getRx0()); // rgHorz.low
         outPacket.encodeShort(getRx1()); // rgHorz.high
